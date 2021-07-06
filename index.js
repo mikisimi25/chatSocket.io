0f2a43e5d44a53313083420b6b8e7d9bc3fae4d4
Maksym <mikisimi25.dev@gmail.com>
2021-07-06 14:46:07 +0200

    First version of the chat

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 let express = require('express');
 let app = express();
-let server = require('http').createServer(app);
-let io = require('socket.io');
+let server = require('http').createServer(app).listen(3000);
+let io = require('socket.io')(server);
 
 app.get('/',(req,res) => {
     res.sendFile(__dirname + "/index.html");
 });
 
-server.listen(3000);
+let users = [], connections = [];
+
+io.sockets.on('connection', (socket) => {
+    console.log("Success connection");
+    connections.push(socket);
+
+    socket.on('disconnect', (data) => {
+        connections.splice(connections.indexOf(socket), 1);
+        console.log("Success disconnection");
+    });
+
+    socket.on('send mess', (data) => {
+        io.sockets.emit('add mess', {mess: data.mess, name: data.name, className: data.className});
+    });
+});
